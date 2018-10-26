091043f04b99b1c6b81a8cf0752e94d160515b27
GunHee5719 <ghj5719@naver.com>
2018-10-26 20:32:57 +0900

    Second project : Jinja2 template using, comments modified

--- BasicStudy2.py
+++ BasicStudy2.py
@@ -1,8 +1,11 @@
 # https://cozy-ho.github.io/flask/2017/10/16/flask-day03.html 페이지를 참고하여 공부 진행.
+
 # 이 파일에서는 신사2 템플릿엔진을 사용하는 것을 다룬다.
 # template 폴더를 생성하여 안에 BasicStudy2.html 파일을 만들었으며, 그 안에는 다음 내용을 담는다.
 # <h1>Hello, {{name}}!</h1>
+
 # {{name}} 부분은 변수를 뜻하며, 렌더링되는 시점에서의 데이터로부터 얻어오는 값을 의미한다. 어떤 타입을 써도 인식한다.
+# 만약 {{name}} 을 {{name | apitalize}} 를 쓴다면, 변수의 첫 문자를 대문자로 한다. 즉, |를 분리자로 하여 필터를 사용할 수 있다.
 
 # Flask 프레임워크를 import하며, Jinja2 template engine 통합을 위해 함께 import 해준다.
 from flask import Flask, render_template
